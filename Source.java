6de8f62dcc48d851b104da4acf3a1fca24cd3d5a
An <junhan.an@fiserv.com>
2023-11-06 14:05:41 -0500

    solved

--- Source.java
+++ Source.java
@@ -10,24 +10,51 @@ class Node<T> {
   
   class Source {
     public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
-      return head;
+      Node<T> previous = null;
+      Node<T> current = head;
+      Node<T> next = null;
+
+      while (current != null) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+      }
+      return previous;
     }
     
     public static void main(String[] args) {
-        Node<String> x = new Node<>("x");
-        Node<String> y = new Node<>("y");
+        // Node<String> x = new Node<>("x");
+        // Node<String> y = new Node<>("y");
     
-        x.next = y; // x -> y
-    
-        reverseList(x); // y -> x
+        // x.next = y; // x -> y
+
+
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        Node<String> e = new Node<>("e");
+        Node<String> f = new Node<>("f");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        // a -> b -> c -> d -> e -> f
+
+        // reverseList(a);
+
+        Node<String> reversedHead = reverseList(a);
+        // reverseList(x); // y -> x
 
         // Printing solution
-        Node<String> head = Source.reverseList(x);
+        // Node<String> head = Source.reverseList(x);
 
-        while (head != null) {
-            System.out.println(head.val);
-            head = head.next;            
+        while (reversedHead != null) {
+            System.out.println(reversedHead.val);
+            reversedHead = reversedHead.next;            
         }
     }
   }
